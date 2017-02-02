9d527fec510725c0ac942660d4cd8d0ab07efae0
Banajit <banajit@visibleaccent.com>
2017-02-02 12:03:10 +0530

    grunt fixed

--- Gruntfile.js
+++ Gruntfile.js
@@ -307,7 +307,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       options: {
         // This should be the name of your apps angular module
-        module: 'angularJsSeedApp',
+        module: 'amberApp',
         htmlmin: {
           collapseBooleanAttributes: true,
           collapseWhitespace: true,

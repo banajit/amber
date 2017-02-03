d489d67faa3d54287239d607e30dd7679064ce17
Banajit <banajit@visibleaccent.com>
2017-02-03 13:00:09 +0530

    updated script

--- client/components/form-builder/form-builder.directive.js
+++ client/components/form-builder/form-builder.directive.js
@@ -5,7 +5,7 @@
 angular.module('amberApp')
   .directive('formBuilder', function () {
     return {
-      templateUrl: 'components/form-builder/form-builder.html',
+      templateUrl: 'components/form-builder/views/form-builder.html',
       restrict: 'E',
       controller: 'formBuilderCtrl'
     };
